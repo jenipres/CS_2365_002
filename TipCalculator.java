4f770fcd71098dd0654f317bb9637e489a0ce674
jenipres <jenipres@ttu.edu>
2025-11-13 11:43:20 -0600

    Create TipCalculator.java

---
+++ TipCalculator.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.util.Scanner;
+
+public class TipCalculator extends Application {
+
+@Override
+public void start(Stage primaryStage)
+{
+	primaryStage.show();
+}
+public static void main(String[] args)
+{
+	launch(args);
+}
+
+}
+
